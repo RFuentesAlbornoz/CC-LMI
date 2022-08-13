99616a896461ad29565ba7dff79f9d0925b6320c
RFuentesAlbornoz <101520218+RFuentesAlbornoz@users.noreply.github.com>
2022-08-12 20:11:36 -0400

    Update BuckController_GainScheduling.m

--- BuckController_GainScheduling.m
+++ BuckController_GainScheduling.m
@@ -1,17 +1,3 @@
-function y = BuckController_GainScheduling(uR,uVin)
+function y = BuckController_GainScheduling(Vin)
 
-Vin=[80 120]; R=[8 12];
-
-u1=(1/uR);
-u2=uVin;
-
-P3 = [1/(max(R)) 1/(min(R))]; %1/R
-P6 = [min(Vin) max(Vin)]; %Vin
-
-
-c1 = (u1-max(P3))/(min(P3)-max(P3));
-d1 = (u2-max(P6))/(min(P6)-max(P6));
-c2 = 1- c1;
-d2 = 1- d1;
-
-y =d1*[-0.11161    -0.240788      42.0461]  + d2*[-0.100536    -0.228288      41.9243] ;
+y =[-0.199284414244108        -0.336374083268391          55.2392529650768] + Vin*[0.00098472049215651       0.00169248226269358         -0.27347163827411];
